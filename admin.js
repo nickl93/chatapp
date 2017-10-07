f1c4576b36de659f6c9d2a7fecacb207d8a6ce98
Nicholas Lambourne <ndl93@live.com>
2017-10-07 19:17:35 +1000

    added favicons

---
+++ admin.js
@@ -0,0 +1,60 @@
+let express = require("express");
+let uuid = require("node-uuid");
+let _ = require("lodash");
+let rooms = require("./data/rooms.json");
+
+let router = express.Router();
+module.exports = router;
+
+router.get('/rooms', function (req, res) {
+    res.render("rooms", {
+        title: "Rooms",
+        rooms: rooms
+    });
+});
+
+router.route('/rooms/admin/add')
+    .get(function (req, res) {
+        res.render("add")
+    })
+    .post(function (req, res) {
+        console.log(req);
+        let room = {
+            name: req.body.name,
+            id: uuid.v4()
+        };
+        console.log(room);
+        rooms.push(room);
+        res.redirect(req.baseUrl + '/rooms');
+    });
+
+router.get('/rooms/delete/:id', function (req, res) {
+    let roomId = req.params.id;
+    rooms = rooms.filter(r => r.id !== roomId);
+    res.redirect(req.baseUrl + "/rooms");
+});
+
+router.route('/rooms/edit/:id')
+    .get(function (req, res) {
+        let roomId = req.params.id;
+        let room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+        res.render("edit", {room: room});
+    })
+    .post(function (req, res) {
+        let roomId = req.params.id;
+        let room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+        room.name = req.body.name;
+        res.redirect(req.baseUrl + "/rooms");
+    });
+
+router.get('/users', function (req, res) {
+    res.render("users", {title: "Users"});
+});
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require("express");
-var rooms = require("./data/rooms.json");
-var uuid = require("node-uuid");
-var bodyParser = require("body-parser");
-var _ = require("lodash");
+let express = require("express");
+let uuid = require("node-uuid");
+let bodyParser = require("body-parser");
+// My Modules
+let adminRouter = require("./admin");
 
-var app = express();
+let app = express();
 
 app.set("views", "views");
 app.set("view engine", "jade");
@@ -15,47 +15,20 @@ app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("node_modules/popper.js/dist"));
 app.use(express.static("node_modules/font-awesome"));
 app.use((bodyParser.urlencoded({ extended: true})));
+app.use(function (req, res, next) {
+   console.log("Incoming request: "+req.url);
+   next();
+});
+// My Modules
+app.use("/admin", adminRouter);
+
 
 app.get('/', function (req, res) {
     res.render("index", { title: "Home"});
 });
 
-app.get('/admin/rooms', function (req, res) {
-    res.render("rooms", { title: "Rooms",
-                          rooms: rooms});
-});
 
-app.get('/admin/rooms/add', function (req, res) {
-    res.render("add");
-});
 
-app.post('/admin/rooms/add', function (req, res) {
-    console.log(req);
-    var room = {
-        name: req.body.name,
-        id: uuid.v4()
-    };
-    console.log(room);
-    rooms.push(room);
-    res.redirect('/admin/rooms');
-});
-
-app.get('/admin/rooms/delete/:id', function (req, res) {
-    let roomId = req.params.id;
-    rooms = rooms.filter(r => r.id !== roomId);
-    res.redirect("/admin/rooms");
-});
-
-app.get('/admin/rooms/edit/:id', function (req, res) {
-    let roomId = req.params.id;
-    let room = _.find(rooms, r => r.id === roomId);
-
-    res.render("edit", {room: room});
-});
-
-app.get('/admin/users', function (req, res) {
-    res.render("users", { title: "Users"});
-});
 
 app.listen(3001, function () {
     console.log("Chat app listening on port 3001");
