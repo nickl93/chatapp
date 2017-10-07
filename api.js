b3cecacfcdeff4568a40600b68054c6ba0706df7
Nicholas Lambourne <ndl93@live.com>
2017-10-08 02:04:51 +1000

    more api

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let rooms = require("./data/rooms.json");
 let messages = require("./data/messages.json");
+let uuid = require("node-uuid");
 let _ = require("lodash");
 
 let router = express.Router();
@@ -10,16 +11,31 @@ router.get("/rooms", function (req, res) {
     res.json(rooms);
 });
 
-router.get("/rooms/:id/messages", function (req, res) {
-    let roomId = req.params.id;
-    let room = _.find(rooms, r => r.id === roomId);
-    let roomMessages = messages.filter(m => m.roomId === roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
-    res.json({
-        room: room,
-        messages: roomMessages
+router.route("/rooms/:id/messages")
+    .all(function (req, res, next) {
+        let roomId = req.params.id;
+        let room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+        res.locals.room = room;
+        next();
+    })
+    .get(function (req, res) {
+        let roomMessages = messages.filter(m => m.roomId === res.locals.room.id);
+        res.json({
+            room: res.locals.room,
+            messages: roomMessages
+        });
+    })
+    .post(function (req, res) {
+        let message = {
+            roomId: res.locals.room.id,
+            text: req.body.text,
+            userId: "44f885e8-87e9-4911-973c-4074188f408a",
+            id: uuid.v4()
+        };
+        messages.push(message);
+        res.sendStatus(200);
     });
-});
