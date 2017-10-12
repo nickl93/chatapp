1a71e838e7591930c931f04e7192f8c12ffe9f18
Nicholas Lambourne <ndl93@live.com>
2017-10-13 01:07:48 +1000

    started auth

--- auth.js
+++ auth.js
@@ -1,11 +1,19 @@
 let express = require("express");
+let passport = require("passport");
 
 let router = express.Router();
 module.exports = router;
 
-router.get('/login', function (req, res) {
-    res.render('login', {});
-});
+router.route('/login')
+    .get(function (req, res) {
+        res.render('login', {});
+    })
+    .post(function (req, res) {
+        passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/login'
+        })
+    });
 
 router.get('/logout', function (req, res) {
 
---
+++ passport-init.js
@@ -0,0 +1,21 @@
+let passport = require("passport");
+let LocalStrategy = require("passport-local").Strategy;
+let users = require(".data/users.json");
+let _ = require("lodash")
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    let user = _.find(users, u => u.name === username);
+    if (!null || user.password !== password) {
+        done(null, false);
+        return;
+    }
+    done(null, user);
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
